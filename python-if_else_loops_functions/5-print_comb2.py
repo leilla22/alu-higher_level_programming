f327f46270c9b01a900c3878b82dd3c1f0869614
leilla22 <i.leilla@alustudent.com>
2025-06-25 08:31:04 -0700

    ifs 5

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,6 @@
-#!/usr/bin/python34
+#!/usr/bin/python3
 for i in range(0, 100):
-    print(f"{i:02}", end=", " if i < 99 else "")
-print()
+    if i <= 98:
+        print("{:02d}".format(i), end=", ")
+    else:
+        print(99)
