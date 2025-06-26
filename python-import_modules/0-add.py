cd3d5c25bd57942006c23eb09e8a54d298f3a0e9
leilla22 <i.leilla@alustudent.com>
2025-06-26 03:00:55 -0700

    function 0

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-a=1
-a=2
-def add(a,b):
-    result= a + b
-    print("a+b=", result")
-add()    
+if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
+print("{} + {} = {}".format(a, b, add(a, b)))
