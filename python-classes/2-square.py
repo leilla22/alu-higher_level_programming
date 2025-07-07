e48cb03f702b8bb61e418ad6f2ec02924990e8fd
leilla22 <i.leilla@alustudent.com>
2025-07-07 04:46:55 -0700

    class 3

---
+++ python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usrbin/python3
+"""Defines a square"""
+
+class Square:
+    """Represent a square"""
+
+    def __init__(self, size=0):
+        """ initializing  the square object """
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = int(size)
