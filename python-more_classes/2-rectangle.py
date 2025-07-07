ab6cc4f50761e0be27d8f9ff8c23f53a0aa36082
leilla22 <i.leilla@alustudent.com>
2025-07-07 05:56:23 -0700

    class 9

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -6,18 +6,18 @@ class Rectangle:
     """ Defines a rectangle """
 
     def __init__(self, width=0, height=0):
-        """ Initialize the variables """
+        """Initialize the variables"""
         self.width = width
         self.height = height
 
     @property
     def width(self):
-        """ Getter method to get the width value """
+        """Getter method to get the width value"""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """ Setter method to set the width value """
+        """Setter method to set the width value"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         elif value < 0:
@@ -37,3 +37,15 @@ class Rectangle:
         elif value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
+
+    def area(self):
+        r_area = self.__width * self.__height
+        return r_area
+
+    def perimeter(self):
+        r_perimeter = 2 * (self.__width + self.__height)
+
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else:
+            return r_perimeter
