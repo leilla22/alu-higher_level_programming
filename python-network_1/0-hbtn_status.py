373427bbe95447d2f83193da56de27cc2e0cd071
leilla22 <i.leilla@alustudent.com>
2025-07-24 03:53:28 -0700

    0-hbtn_status.py

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""Fetches http://0.0.0.0:5050/status using urllib"""
+"""Fetches https://intranet.hbtn.io/status using urllib"""
 
 import urllib.request
 
 if __name__ == "__main__":
-    url = "http://0.0.0.0:5050/status"
+    url = "https://intranet.hbtn.io/status"
 
     with urllib.request.urlopen(url) as response:
         body = response.read()
